504610bce0862ac34c56c3b854bd41bbd050cd25
sharmane burlaza <sjmburlaza@gmail.com>
2023-06-10 17:36:22 +0800

    Change const variables to readonly in auth service

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -3,28 +3,28 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { LoginInfo, RegisterInfo } from '../models/model';
 
-const AUTH_API = 'http://localhost:4000/api/user/';
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  readonly AUTH_API = 'http://localhost:4000/api/user/';
+  readonly httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(private http: HttpClient) { }
 
   login(loginInfo: LoginInfo): Observable<any> {
-    return this.http.post(AUTH_API + 'login', loginInfo, httpOptions);
+    return this.http.post(this.AUTH_API + 'login', loginInfo, this.httpOptions);
   }
 
   emailExists(email: string): Observable<any> {
-    return this.http.post(AUTH_API + 'email-exists', { email }, httpOptions);
+    return this.http.post(this.AUTH_API + 'email-exists', { email }, this.httpOptions);
   }
 
   register(registerInfo: RegisterInfo): Observable<any> {
-    return this.http.post(AUTH_API + 'register', registerInfo, httpOptions);
+    return this.http.post(this.AUTH_API + 'register', registerInfo, this.httpOptions);
   }
 
 }
