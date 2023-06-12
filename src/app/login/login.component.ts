6332fad3a80eeeba2f474f1db11e728fb482e3ec
sharmane burlaza <sjmburlaza@gmail.com>
2023-06-12 18:44:20 +0800

    Implement login page layout; move comp css to global

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
+import { TokenStorageService } from '../shared/services/token-storage.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form!: FormGroup<any>;
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
 
+  constructor(
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService,
+    private router: Router,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    // if (this.tokenStorage.getToken()) {
+    //   this.isLoggedIn = true;
+    // }
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(32)]],
+    });
+  }
+
+  onSubmit(): void {
+    // hello
+  }
 }
